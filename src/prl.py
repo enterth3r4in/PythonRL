260c61368de844c3e4cf1627c5b13e40cbb475d8
IntoTheRain <intotherain.ccgames@gmail.com>
2015-10-27 23:53:37 -0400

    Playing around

---
+++ src/prl.py
@@ -0,0 +1,14 @@
+#Imports
+import libtcodpy as ltc
+
+def function_name(arg1, arg2):
+    """Describe the function purpose
+    
+    Parameters:
+        arg1 - described
+        arg2 - described
+        
+    Returns:
+        Describe what is returned
+    """
+    return None
